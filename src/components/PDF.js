22a384bcff1f800da532d8c33298e91b577dc0aa
Adrian Américo <1fnadrian1@gmail.com>
2021-07-26 15:12:44 -0300

    estrutura front-end e instalacao de bibliotecas essenciais

--- src/components/PDF.js
+++ src/components/PDF.js
@@ -1,14 +1,17 @@
+import { PDFViewer } from '@react-pdf/renderer';
 import React from 'react';
 import Pdf from 'react-to-pdf';
 import PdfPage from '../Pages/PdfPage';
 
 const ref = React.createRef();
 
-const PDF = (props) => {
+function PDF() {
     return (
         <>
             <div ref={ref}>
-                <PdfPage />
+                {/* <PDFViewer width="800px" height="600px"> */}
+                    <PdfPage />
+                {/* </PDFViewer> */}
             </div>
             <Pdf targetRef={ref} filename="teste.pdf">
                 {({ toPdf }) => <button onClick={toPdf}>capture</button>}
